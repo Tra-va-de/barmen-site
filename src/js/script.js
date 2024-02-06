af4c1a338d3cba9f5442dbb387ddd3ada3abea65
Tra-va-de <distraction.mansion@gmail.com>
2024-02-06 12:32:31 +0300

    Сделали часть секции about с информацией.

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,31 @@
+// Переменные, с которыми предстоит работа
+const nav = document.querySelector('.nav');
+const aboutDescription = document.querySelector('.about__description');
+
 // Функция открытия / закрытия навигации на экранах
 // небольшого размера
-const nav = document.querySelector('.nav');
-
 const openCloseNav = () => {
     nav.classList.toggle('show');
 }
 
 // Прослушка изменения размеров экрана
 // для удаление класса hide у nav 
-window.addEventListener('resize', function () {
+window.addEventListener('resize', () => {
     if (window.innerWidth > 768 && nav.classList.contains('show')) {
         nav.classList.remove('show');
     }
 });
+
+// Создали обсервер для проверки появления элемента
+// в зоне видимости и добавления ему класса show
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+// Передали элемент для проверки
+observer.observe(aboutDescription);
